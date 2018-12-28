8da5ef582d43fc424d628f795facea2d35204e6d
24Preacher <pedro98medeiros@gmail.com>
2018-12-28 15:45:45 +0000

    Classe encomenda

--- Projeto/src/Configuracao.java
+++ Projeto/src/Configuracao.java
@@ -1,103 +1,1 @@
-import java.awt.*;
-import java.util.Map;
-import java.util.Objects;
-import java.util.TreeMap;
-
-public class Configuracao {
-
-    private Carro carro;
-    private Map<Integer, Componente> configcar;
-    private Componente componente;
-
-    public Configuracao(){
-        this.carro = null;
-        this.configcar = new TreeMap<Integer, Componente>();
-        this.componente = null;
-    }
-
-    public Configuracao(Carro a, Map<Integer,Componente> c, Componente s){
-        this.carro = a;
-        setConfigCar(c);
-        this.componente = s;
-    }
-
-
-    public  Configuracao(Configuracao c){
-        this.carro = c.getCarro();
-        this.configcar = c.getConfigCar();
-        this.componente = c.getComponente();
-    }
-
-    // GETS
-
-
-    public Carro getCarro() {
-        return carro;
-    }
-
-    public Map<Integer, Componente> getConfigCar(){
-        Map<Integer,Componente> novo = new TreeMap<Integer, Componente>();
-        for(Map.Entry<Integer,Componente> i : this.configcar.entrySet())
-            novo.put(i.getKey(), i.getValue());
-        return novo;
-    }
-
-    public Componente getComponente(){
-        return this.componente;
-    }
-
-    //SETS
-
-
-    public void setCarro(Carro carro) {
-        this.carro = carro;
-    }
-
-    public void setConfigCar(Map<Integer, Componente> c){
-        this.configcar = new TreeMap<Integer, Componente>();
-
-        for(Map.Entry<Integer,Componente> i : c.entrySet())
-            this.configcar.put(i.getKey(), i.getValue());
-    }
-
-    public void setComponente(Componente c){
-        this.componente = c;
-    }
-
-    //CLONE
-
-    public Configuracao clone(){
-        return new Configuracao(this);
-    }
-
-    //EQUALS
-
-
-    public boolean equals(Object o){
-        if (o == this) return true;
-        if ((o == null) || (o.getClass() != this.getClass())) return false;
-
-        Configuracao c = (Configuracao) o;
-        return (this.carro.equals(c.getCarro()) &&
-                this.configcar.equals((c.getConfigCar())) &&
-                this.componente.equals(c.getComponente()));
-    }
-
-    //toString
-
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("-----Configuração-----");
-        for(Componente c : this.configcar.values())
-            sb.append(c.toString());
-        return sb.toString();
-    }
-
-    public void insereComponente (Componente c){
-        if(this.configcar.containsKey(c.getTipo())){
-            this.configcar.replace(c.getTipo(), c);
-        } else {
-            this.configcar.put(c.getTipo(),c);
-        }
-    }
-}
+import java.util.Map;import java.util.TreeMap;public class Configuracao {    private Carro carro;    private Map<Integer, Componente> configcar;    private Componente componente;    public Configuracao(){        this.carro = null;        this.configcar = new TreeMap<Integer, Componente>();        this.componente = null;    }    public Configuracao(Carro a, Map<Integer,Componente> c, Componente s){        this.carro = a;        setConfigCar(c);        this.componente = s;    }    public  Configuracao(Configuracao c){        this.carro = c.getCarro();        this.configcar = c.getConfigCar();        this.componente = c.getComponente();    }    // GETS    public Carro getCarro() {        return carro;    }    public Map<Integer, Componente> getConfigCar(){        Map<Integer,Componente> novo = new TreeMap<Integer, Componente>();        for(Map.Entry<Integer,Componente> i : this.configcar.entrySet())            novo.put(i.getKey(), i.getValue());        return novo;    }    public Componente getComponente(){        return this.componente;    }    //SETS    public void setCarro(Carro c) {        this.carro = c;    }    public void setConfigCar(Map<Integer, Componente> c){        this.configcar = new TreeMap<Integer, Componente>();        for(Map.Entry<Integer,Componente> i : c.entrySet())            this.configcar.put(i.getKey(), i.getValue());    }    public void setComponente(Componente c){        this.componente = c;    }    //CLONE    public Configuracao clone(){        return new Configuracao(this);    }    //EQUALS    public boolean equals(Object o){        if (o == this) return true;        if ((o == null) || (o.getClass() != this.getClass())) return false;        Configuracao c = (Configuracao) o;        return (this.carro.equals(c.getCarro()) &&                this.configcar.equals((c.getConfigCar())) &&                this.componente.equals(c.getComponente()));    }    //toString    public String toString(){        StringBuilder sb = new StringBuilder();        sb.append("-----Configuração-----");        for(Componente c : this.configcar.values())            sb.append(c.toString());        return sb.toString();    }    public void insereComponente (Componente c){        if(this.configcar.containsKey(c.getTipo())){            this.configcar.replace(c.getTipo(), c);        } else {            this.configcar.put(c.getTipo(),c);        }    }}
---
+++ Projeto/src/Encomenda.java
@@ -0,0 +1,123 @@
+public class Encomenda {
+
+    private int id;
+    private String nomeCliente;
+    private int nifCliente;
+    private double preco;
+    private Funcionario funcionario;
+    private Configuracao configuracao;
+
+    public Encomenda(){
+        this.id = 0;
+        this.nomeCliente = null;
+        this.nifCliente = 0;
+        this.preco = 0;
+        this.funcionario = null;
+        this.configuracao = null;
+    }
+
+    public Encomenda(int a, String b, int c, double d, Funcionario e, Configuracao f){
+        this.id = a;
+        this.nomeCliente = b;
+        this.nifCliente = c;
+        this.preco = d;
+        this.funcionario = e;
+        this.configuracao = f;
+    }
+
+    public Encomenda(Encomenda e){
+        this.id = e.getId();
+        this.nomeCliente = e.getNomeCliente();
+        this.nifCliente = e.getNifCliente();
+        this.preco = e.getPreco();
+        this.funcionario = e.getFuncionario();
+        this.configuracao = e.getConfiguracao();
+    }
+
+    //GETS
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public int getNifCliente() {
+        return nifCliente;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public Configuracao getConfiguracao() {
+        return configuracao;
+    }
+
+    //SETS
+
+    public void setId(int a) {
+        this.id = a;
+    }
+
+    public void setNomeCliente(String a) {
+        this.nomeCliente = a;
+    }
+
+    public void setNifCliente(int a) {
+        this.nifCliente = a;
+    }
+
+    public void setPreco(double a) {
+        this.preco = a;
+    }
+
+    public void setFuncionario(Funcionario a) {
+        this.funcionario = a;
+    }
+
+    public void setConfiguracao(Configuracao a) {
+        this.configuracao = a;
+    }
+
+    //CLONE
+
+    public Encomenda clone(){
+        return new Encomenda(this);
+    }
+
+    //toSTRING
+
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Encomenda{");
+        sb.append("id=").append(id);
+        sb.append(", nomeCliente='").append(nomeCliente).append('\'');
+        sb.append(", nifCliente=").append(nifCliente);
+        sb.append(", preco=").append(preco);
+        sb.append(", funcionario=").append(funcionario);
+        sb.append(", configuracao=").append(configuracao);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    // EQUALS
+
+    public boolean equals(Object o){
+        if(o == this) return true;
+        if((o == null) || (o.getClass() != this.getClass())) return false;
+
+        Encomenda e = (Encomenda) o;
+        return (e.getId() == this.getId() &&
+                e.nomeCliente.equals(this.getNomeCliente()) &&
+                e.getNifCliente() == this.getNifCliente() &&
+                e.getPreco() == this.getPreco() &&
+                e.getFuncionario().equals(this.getFuncionario()) &&
+                e.getConfiguracao().equals(this.getConfiguracao()));
+    }
+}
